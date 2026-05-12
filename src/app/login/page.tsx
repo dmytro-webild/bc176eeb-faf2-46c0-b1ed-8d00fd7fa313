a9b94026ade20b1dbd8367609f65913bb0238c02
bender <kudin.dima@gmail.com>
2026-05-12 11:14:47 +0000

    Update src/app/login/page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -25,6 +25,7 @@ export default function LoginPage() {
         navItems={[
             { name: "Home", id: "/" },
             { name: "Inventory", id: "/inventory" },
+            { name: "Add Vehicle", id: "/admin/add-vehicle" },
             { name: "Login", id: "/login" }
         ]}
         brandName="Right Drive Auto"

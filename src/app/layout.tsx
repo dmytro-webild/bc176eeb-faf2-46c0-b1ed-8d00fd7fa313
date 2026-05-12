39e6a9e8fb348906d0d1e19551b69257bd0297e7
dk <kudin.dima@gmail.com>
2026-05-12 09:12:45 +0000

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,40 @@
+import type { Metadata } from "next";
+import { Halant } from "next/font/google";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import "@/lib/gsap-setup";
+import { ServiceWrapper } from "@/components/ServiceWrapper";
+import Tag from "@/tag/Tag";
+import { getVisualEditScript } from "@/utils/visual-edit-script";
+import { Public_Sans } from "next/font/google";
+
+
+
+export const metadata: Metadata = {
+  title: 'Right Drive Auto | Quality Pre-Owned Vehicles',
+  description: 'Find quality pre-owned vehicles at Right Drive Auto. Easy financing, multi-point inspections, and friendly service.',
+};
+
+const publicSans = Public_Sans({ variable: "--font-public-sans", subsets: ["latin"], });
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <ServiceWrapper>
+        <body className={`${publicSans.variable} antialiased`}>
+          <Tag />
+          {children}
+          <script
+              dangerouslySetInnerHTML={{
+                  __html: `${getVisualEditScript()}`
+              }}
+          />
+        </body>
+      </ServiceWrapper>
+    </html>
+  );
+}

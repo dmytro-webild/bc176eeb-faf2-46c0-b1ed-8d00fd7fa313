3ca5eafec96852789a2d989d53813e1de789dd40
bender <kudin.dima@gmail.com>
2026-06-03 02:18:20 +0000

    Update src/app/login/page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -23,10 +23,16 @@ export default function LoginPage() {
     >
       <NavbarStyleFullscreen
         navItems={[
-            { name: "Home", id: "/" },
-            { name: "Inventory", id: "/inventory" },
-            { name: "Add Vehicle", id: "/admin/add-vehicle" },
-            { name: "Login", id: "/login" }
+          { name: "Home", id: "/" },
+          { name: "Features", id: "features" },
+          { name: "Inventory", id: "/inventory" },
+          { name: "Store Hours", id: "#hours" },
+          { name: "Metrics", id: "metrics" },
+          { name: "Testimonials", id: "testimonials" },
+          { name: "FAQ", id: "faq" },
+          { name: "Contact", id: "contact" },
+          { name: "Login", id: "/login" },
+          { name: "Add Vehicle", id: "/admin/add-vehicle" }
         ]}
         brandName="Right Drive Auto"
       />
